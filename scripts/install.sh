85f90e22532d48b0700ffb0cfbb3c87f5d764e15
cocacola0 <pinbu.studio@gmail.com>
2022-07-18 21:36:32 +0300

    Added setup scripts

---
+++ scripts/install.sh
@@ -0,0 +1,28 @@
+# Script that installs openvino on Raspberry Pi
+# host OS = 32 bit "RASPBERRY PI OS(LEGACY)"
+
+#Downnload OpenVino 2019.3 and pretrained models for test
+sudo mkdir models && cd models/
+wget https://download.01.org/opencv/2019/openvinotoolkit/R3/l_openvino_toolkit_runtime_raspbian_p_2019.3.334.tgz
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.bin
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.xml
+
+#Install cmake
+sudo apt install -y cmake
+
+#Create install folder
+sudo mkdir /opt/intel
+sudo mkdir /opt/intel/mkdir
+
+#Extract
+sudo tar -xvzf l_openvino_toolkit_runtime_raspbian_p_2019.3.334.tgz --strip 1 -C /opt/intel/openvino
+sudo rm -rf l_openvino_toolkit_runtime_raspbian_p_2019.3.334.tgz
+
+#Setup openvino
+source /opt/intel/openvino/bin/setupvars.sh
+echo "source /opt/intel/openvino/bin/setupvars.sh" >> ~/.bashrc
+
+#Setup USB rules
+sudo usermod -a -G users "$(whoami)"
+
+#Reset the Raspberry Pi
---
+++ scripts/test_installation.sh
@@ -0,0 +1,12 @@
+# Script that tests installation of openvino on Raspberry Pi
+
+#Create build folder
+mkdir build && cd build
+
+#Build object_detection_sample_ssd
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-march=armv7-a" /opt/intel/openvino/deployment_tools/inference_engine/samples
+make -j2 object_detection_sample_ssd
+
+#Perform inference
+cd ..
+build/armv7l/Release/object_detection_sample_ssd -m models/face-detection-adas-0001.xml -d MYRIAD -i images/face.jpeg
